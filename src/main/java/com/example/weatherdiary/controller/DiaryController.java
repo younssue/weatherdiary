926daf5b8ab7ef7dee5d4b1dacb843b2ddfe0449
younssue <love_rvd7@naver.com>
2023-11-23 17:43:40 +0900

    :tada: 날씨 api 일기 조회 기능 구현

--- src/main/java/com/example/weatherdiary/controller/DiaryController.java
+++ src/main/java/com/example/weatherdiary/controller/DiaryController.java
@@ -1,5 +1,6 @@
 package com.example.weatherdiary.controller;
 
+import com.example.weatherdiary.domain.Diary;
 import com.example.weatherdiary.service.DiaryService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +22,18 @@ public class DiaryController {
                      @RequestBody String text){
         diaryService.createDiary(date,text);
     }
+
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate date ){
+        return diaryService.readDiary(date);
+    }
+
+    @GetMapping("/read/diaries")
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) LocalDate endDate){
+        return diaryService.readDiaries(startDate,endDate);
+    }
+
+
 
 }
--- src/main/java/com/example/weatherdiary/service/DiaryService.java
+++ src/main/java/com/example/weatherdiary/service/DiaryService.java
@@ -16,6 +16,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -42,6 +43,14 @@ public class DiaryService {
         nowDiary.setText(text);
         nowDiary.setDate(date);
         diaryRepository.save(nowDiary);
+    }
+
+    public List<Diary> readDiary(LocalDate date) {
+        return diaryRepository.findAllByDate(date);
+    }
+
+    public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate) {
+        return diaryRepository.findAllByDateBetween(startDate, endDate);
     }
 
     private String getWeatherString() {
@@ -92,4 +101,7 @@ public class DiaryService {
 
         return resultMap;
     }
+
+
+
 }
