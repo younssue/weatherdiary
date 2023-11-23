1d45d9e1227d59c9c27c052fe38b4ccc4236c3cb
younssue <love_rvd7@naver.com>
2023-11-23 16:42:39 +0900

    :tada: 날씨 api 일기 작성 프로젝트 생성

---
+++ src/main/java/com/example/weatherdiary/WeatherdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherdiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weatherdiary/WeatherdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
